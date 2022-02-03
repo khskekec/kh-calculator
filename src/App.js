e7b19a458443452b03d0bd675b66057addc98bc6
Selcuk Kekec <skekec@i-doit.com>
2022-02-03 21:11:05 +0100

    Project initialized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import {useMemo, useState} from "react";
+import classname from 'classname';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [carbonhydrates, setCarbonHydrates] = useState();
+    const [weight, setWeight] = useState();
+    const result = useMemo(() => carbonhydrates / 100 * weight, [carbonhydrates, weight]);
+
+    console.log({carbonhydrates, weight, result});
+    return (
+        <div className='d-flex'>
+            <div className='container-fluid container-md mt-auto'>
+                <div className="mb-5">
+                    <label htmlFor="carbonhydratesInput" className="form-label fw-bold">Kohlenhydrate pro 100g</label>
+                    <input type="number" className="form-control form-control-lg" id="carbonhydratesInput"
+                           placeholder="Kohlenhydrate pro 100g" value={carbonhydrates}
+                           onChange={e => setCarbonHydrates(e.target.value)}/>
+                </div>
+                <div className="mb-5">
+                    <label htmlFor="weightInput" className="form-label fw-bold">Gewicht in Gramm</label>
+                    <input type="number" className="form-control form-control-lg" id="weightInput"
+                           placeholder="Gewicht in Gramm" value={weight} onChange={e => setWeight(e.target.value)}/>
+                </div>
+                <div className={classname('text-center badge bg-secondary d-flex', {'bg-danger': !result || result === 0})}
+                     style={{whitespce: 'break-spaces', padding: 50}}>
+                    <h1 className='mb-0 text-center w-100'>{result ? result.toLocaleString('de-DE', {minimumFractionDigits: 0}) + ' KH' : 'Bitte beide Felder ausfüllen!'}</h1>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
